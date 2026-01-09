bdbad1db1e6dd6106853f082cc1251c4396f66b0
andreichenko256 <andreichenko.kirill2000@gmail.com>
2026-01-09 18:29:13 +0200

    chore: project set up

--- TrackIt/ViewController.swift
+++ TrackIt/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  TrackIt
-//
-//  Created by Kyrylo Andreichenko on 09.01.2026.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
-
